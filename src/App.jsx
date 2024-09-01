552aae90f18717fd70ab7937260513d7a683c65f
ishratjahanesha21 <ishratjahanesha21@gmail.com>
2024-09-01 13:04:01 +0600

    okaay

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Accrodiand from './Projects/Project-1/Accrodiand'
+import Starreting from './Projects/Project-2/Starreting'
 import Rendomcolour from './Random-colour-generator/Rendomcolour'
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
     react 25 project mini
     <h1>Project-1</h1>
     <Accrodiand/>
+    <h1>Project-2</h1>
+    <Starreting/>
     <Rendomcolour/>
     </>
   )
